9ff192c5650563e9123a34c90b43ca2c55b547ef
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-11-27 17:02:09 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 
+typedef struct
+{
+   char* majorColor;
+   char* minorColor;
+}colorCode;
+
+colorCode colorCode_t;
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
@@ -16,6 +23,7 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert((colorCode_t-> majorColor[0] =='White') && (colorCode_t.minorColor[1] =='Orange');
     printf("All is well (maybe!)\n");
     return 0;
 }
