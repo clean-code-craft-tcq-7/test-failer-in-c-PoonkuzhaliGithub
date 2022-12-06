75bb02520a19d27075cc69ec2c7016d1cc64cdfb
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-12-06 11:05:58 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* majorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* minorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-int numberOfMajorColors =
-    sizeof(majorColorNames) / sizeof(majorColorNames[0]);
-
-int numberOfMinorColors =
-    sizeof(minorColorNames) / sizeof(minorColorNames[0]);
-
-typedef struct {
-    enum MajorColor majorColorValue;
-    enum MinorColor minorColorValue;
-} ColorPair;
+#include <misaligned.h>
 
 int printColorMap() {
 
